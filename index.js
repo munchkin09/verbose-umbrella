263fc406e56119e50b97007246baacbad4504691
Carlos L <carlos.developing@gmail.com>
2018-11-14 22:36:38 +0100

    first version of updater and get dns script

--- index.js
+++ index.js
@@ -2,40 +2,14 @@
 const publicIp = require('public-ip');
 const fs = require('fs');
 const rpn = require('request-promise-native')
-const domains = ['carloscacharreo.xyz','api.carloscacharreo.xyz','chat.carloscacharreo.xyz','ssh.carloscacharreo.xyz',] 
-var options = {
-    url: 'https://api.cloudflare.com/client/v4/zones/968a8c0efcb81e720783a14683cc3d2c/dns_records',
-    headers: require('./headers'),
-    /*body: {
-        'type':'A',
-        'name':'',
-        'content':'',
-        'ttl':120,
-        'proxied':false}*/
-  };
+const domains = require('./my_zone_identifier.json') 
+const headers = require('./headers')
 var myJson = {
     ip: ''
 };
 
 
 
-rpn.get(options)
-            .then(function (htmlString) {
-                 response =  JSON.parse(htmlString)
-                 console.log(htmlString);
-            })
-            .catch(function (err) {
-                // Crawling failed...
-                console.log(err);
-                
-            });
-
-
-/*publicIp.v4({ http: true, timeout: 2000 }).then(ip => {
-    console.log('1st publicIp =>' + ip)
-    myJson.ip = ip;
-    fs.writeFile( 'ip.json', JSON.stringify( myJson ), 'utf8', yourCallback );
-});
 
 
 
@@ -44,12 +18,19 @@ function doWork() {
     console.log('doWork empieza!' + json.ip)
     publicIp.v4()
     .then(ip => {
-        if(ip === json.ip) return 0
-
-        options.body.content = ip
-        domains.map(() => {
-            options.body.name
-
+        if(ip === domains[0].content) return 0
+        domains.map((element) => {
+            var options = {
+                url: 'https://api.cloudflare.com/client/v4/zones/968a8c0efcb81e720783a14683cc3d2c/dns_records/' + element.id,
+                headers: headers,
+                body: {
+                    'type':'A',
+                    'name':element.name,
+                    'content':ip,
+                    'ttl':120,
+                    'proxied':element.name == 'ssh.carloscacharreo.xyz' || element.name == 'chat.carloscacharreo.xyz' ? false : true }
+              };    
+            console.log(JSON.stringify(options,null,2))
             rpn.put(options)
             .then(function (htmlString) {
                  response =  JSON.parse(htmlString)
@@ -70,7 +51,13 @@ function doWork() {
 function yourCallback(err, data) {
     if(err) process.exit(0)
     setInterval(doWork,10000)
-}*/
+}
+
+publicIp.v4({ http: true, timeout: 2000 }).then(ip => {
+    console.log('1st publicIp =>' + ip)
+    myJson.ip = ip;
+    fs.writeFile( 'ip.json', JSON.stringify( myJson ), 'utf8', yourCallback );
+});
 
 
 
---
+++ scriptGetDns.js
@@ -0,0 +1,37 @@
+const publicIp = require('public-ip');
+const fs = require('fs');
+const rpn = require('request-promise-native')
+
+function getDnsRecords() {
+    return new Promise(function(resolve, reject) {
+        var options = {
+            url: 'https://api.cloudflare.com/client/v4/zones/968a8c0efcb81e720783a14683cc3d2c/dns_records',
+            headers: require('./headers'),
+          };
+        rpn.get(options)
+        .then(function (htmlString) {
+             response =  JSON.parse(htmlString)
+             
+             
+             const domains = response.result.filter((elem) => {
+                return elem.type === 'A'
+             })
+             console.log(JSON.stringify(domains,null,2));
+             resolve(domains)
+        })
+        .catch(function (err) {
+            // Crawling failed...
+            console.log(err);
+            reject(err);
+        });
+    })
+    
+}
+
+getDnsRecords()
+.then((domains) => {
+    fs.writeFile('my_zone_identifier.json',JSON.stringify(domains), {} ,(err) => {
+        if(err) console.log(err); process.exit(-2)
+        process.exit(1)
+    })
+});
